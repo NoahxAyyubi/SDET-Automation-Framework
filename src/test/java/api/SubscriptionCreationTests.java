0a65f6389a38ce905aba16de8ea03ff411c05d64
Noah Muhammad <noahxmuhammad@gmail.com>
2026-02-11 05:49:18 -0500

    KAN-13: add initial subscription API test class skeleton

--- src/test/java/api/SubscriptionCreationTests.java
+++ src/test/java/api/SubscriptionCreationTests.java
@@ -1,5 +1,8 @@
 package test.java.api;
 
 public class SubscriptionCreationTests {
-    
+       
+    public void shouldCreateSubscriptionSuccessfully() {
+        System.out.println("Placeholder test for subscription creation");
+    }
 }
